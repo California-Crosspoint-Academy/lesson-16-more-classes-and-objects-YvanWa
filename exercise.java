95048accd98774794b3f4fd856865c5bd31affd9
YvanWa <yvanwang26@crosspointacademy.org>
2025-10-24 15:30:37 -0700

    commit

---
+++ exercise.java
@@ -0,0 +1,40 @@
+public class exercise{
+    public static void main(String[]args){
+        // 1. true
+        // 2. false
+        // 3. false
+        // 4. false
+        // 5. false
+
+        // 6. Illegal String cannot be converted to int
+        // 7. Illegal method2 has private access in Weenie
+        // 8. Legal
+        // 9. Illegal method method3 in class Weenie cannot be applied to given types
+        // 10. Illegal z has private access in Weenie
+        // 11. Legal
+
+        // 12.
+        // Surfer surferDude;
+        // surferDude = new Surfer();
+
+        // 13. C. Both a and b
+        // 14. 2500
+        // 15. Illegal (NullPointerException: variable 'b' might not have been initialized)
+        // 16. Illegal (Type mismatch: String cannot be converted to a number)
+
+        // 17. (Printed output)
+        // true
+        // true
+        // true
+        // true
+        // false
+        // 1. B
+        // 2. D
+        // 3. C
+        // 4. A
+        // 5. E
+
+
+    }
+
+}
